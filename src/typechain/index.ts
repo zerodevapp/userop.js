a68c9e27593f0a587c4cbe34b05b3b48a3bdf771
Zobeir Hamid <zobeirhamid@berkeley.edu>
2023-05-25 18:53:51 -0700

    feat: adds kernel v1 abis

--- src/typechain/index.ts
+++ src/typechain/index.ts
@@ -2,9 +2,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { EntryPoint } from "./EntryPoint";
+export type { Kernel } from "./Kernel";
+export type { KernelFactory } from "./KernelFactory";
 export type { SimpleAccount } from "./SimpleAccount";
 export type { SimpleAccountFactory } from "./SimpleAccountFactory";
 export * as factories from "./factories";
 export { EntryPoint__factory } from "./factories/EntryPoint__factory";
+export { Kernel__factory } from "./factories/Kernel__factory";
+export { KernelFactory__factory } from "./factories/KernelFactory__factory";
 export { SimpleAccount__factory } from "./factories/SimpleAccount__factory";
 export { SimpleAccountFactory__factory } from "./factories/SimpleAccountFactory__factory";
